2426a3697bb368c4af6ef905b63da576beed3833
Elisha Crisologo <elishacrisologoo@gmail.com>
2022-11-07 17:49:00 +0800

    added Navigation Drawer

---
+++ navigation_drawer/app/src/main/java/com/example/navigation_drawer/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.navigation_drawer
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.navigation_drawer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.account -> Toast.makeText(applicationContext,"Clicked Account",Toast.LENGTH_SHORT).show()
+                R.id.themes -> Toast.makeText(applicationContext,"Clicked Themes",Toast.LENGTH_SHORT).show()
+                R.id.settings -> Toast.makeText(applicationContext,"Clicked Settings",Toast.LENGTH_SHORT).show()
+                R.id.feedback -> Toast.makeText(applicationContext,"Clicked Feedback",Toast.LENGTH_SHORT).show()
+                R.id.faq -> Toast.makeText(applicationContext,"Clicked FAQ",Toast.LENGTH_SHORT).show()
+
+
+            }
+
+            true
+
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)){
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+}
